4107f181291d5c9a2ca2104a2bb65ac86f7573d7
ra1n <64748227+spacepilotAV@users.noreply.github.com>
2021-09-02 17:27:53 -0400

    magic

--- untether/build.sh
+++ untether/build.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-rm -r .DS_Store
-rm -r */.DS_Store
-rm -r */*/.DS_Store
-rm -r */*/*/.DS_Store
-rm -r */*/*/*/.DS_Store
-rm -r */*/*/*/*/.DS_Store
-rm -r */*/*/*/*/*/.DS_Store
+rm -r .*DS_Store
+rm -r */.*DS_Store
+rm -r **/.*DS_Store
+rm -r **/*/.*DS_Store
 sudo chown 0:0 BUILD/daibutsu
 dpkg-deb --build -Zgzip BUILD package/
