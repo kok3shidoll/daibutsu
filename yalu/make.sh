cf768c8d05ae2910899c13eec4eb90fb339f2d23
Dora2 <42214504+dora2-iOS@users.noreply.github.com>
2022-01-15 00:51:45 +0900

    wtf?!

--- yalu/make.sh
+++ yalu/make.sh
@@ -10,6 +10,6 @@ rm amfistop64
 rm untether64
 
 # build 32bit amfistop for 64bit devices
-gcc -std=c++14 cs_tramp.s untether64.mm -o amfistop64 -w -arch armv7 -isysroot "$(xcrun --show-sdk-path --sdk iphoneos)" -framework IOKit -framework Foundation -lz
+gcc -std=c++14 untether64.mm -o amfistop64 -w -arch armv7 -isysroot "$(xcrun --show-sdk-path --sdk iphoneos)" -framework IOKit -framework Foundation -lz
 #strip amfistop64
 ldid -Se.xml amfistop64
